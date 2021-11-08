be3d07a0c7e3a2035221e095dee6b282fa82fb9f
Angeredspy <devygraham@gmail.com>
2021-11-08 15:51:31 -0500

    changes test file from 2 units to 1 integration

--- react-testing-starter-main/src/components/TransactionCreateStepTwo.test.js
+++ react-testing-starter-main/src/components/TransactionCreateStepTwo.test.js
@@ -2,15 +2,10 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event"; 
 import TransactionCreateStepTwo from "./TransactionCreateStepTwo";
 
-test('On initial render the Pay button is disabled', async () => {
-    render(<TransactionCreateStepTwo sender={{id: '5'}} receiver={{id: '5'}}/>) 
-
-    expect(await screen.findByRole('button',{name: /pay/i})).toBeDisabled();
-}); 
-
 test("If an amount and note is entered, the pay button becomes enabled", async () => {
   render(<TransactionCreateStepTwo sender={{ id: "5" }} receiver={{ id: "5" }} />);
 
+  expect(await screen.findByRole("button", { name: /pay/i })).toBeDisabled();
   // screen.getByRole(''); allows us to see all the roles on the screen in an html format
 
   userEvent.type(screen.getByPlaceholderText(/amount/i), "50"); //Here we mimic a userEvent of the user typing "50"
