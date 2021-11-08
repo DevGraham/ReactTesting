e62ea3f28834c3c323dc0fdb8a04b4a8c513f78c
Angeredspy <devygraham@gmail.com>
2021-11-08 00:15:58 -0500

    Adds second test case with typing userEvents :))

--- react-testing-starter-main/src/components/TransactionCreateStepTwo.test.js
+++ react-testing-starter-main/src/components/TransactionCreateStepTwo.test.js
@@ -1,8 +1,20 @@
 import { render, screen } from "@testing-library/react"; 
+import userEvent from "@testing-library/user-event"; 
 import TransactionCreateStepTwo from "./TransactionCreateStepTwo";
 
 test('On initial render the Pay button is disabled', async () => {
     render(<TransactionCreateStepTwo sender={{id: '5'}} receiver={{id: '5'}}/>) 
 
     expect(await screen.findByRole('button',{name: /pay/i})).toBeDisabled();
-})
+}); 
+
+test("If an amount and note is entered, the pay button becomes enabled", async () => {
+  render(<TransactionCreateStepTwo sender={{ id: "5" }} receiver={{ id: "5" }} />);
+
+  // screen.getByRole(''); allows us to see all the roles on the screen in an html format
+
+  userEvent.type(screen.getByPlaceholderText(/amount/i), "50"); //Here we mimic a userEvent of the user typing "50"
+  userEvent.type(screen.getByPlaceholderText(/add a note/i), "Dinner"); //Here we mimic a userEvent of the user typing "Dinner"
+
+  expect(await screen.findByRole("button", { name: /pay/i })).toBeEnabled(); //We expect the button to be enabled after mimicking the above user events.
+});
